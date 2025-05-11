5282a813319621ff0190896beea319637292ad87
Mugara250 <mbonyumugara52@gmail.com>
2025-05-11 19:44:11 +0200

    basic fetch functionality

--- 1-basic-fetch.js
+++ 1-basic-fetch.js
@@ -2,26 +2,17 @@
 const url = 'https://jsonplaceholder.typicode.com/users';
 
 export function getData() {
-  //
-  fetch(url)
-    .then((resp) => {
-      // console.log(resp);
-      //error checking
-      //200-299
-      if (!resp.ok) throw new Error('was not a valid response');
-      return resp.json(); //method to extract JSON string and convert it to an Object
-    })
-    .then((dataobj) => {
-      console.log(dataobj);
-    })
-    .catch((err) => {
-      console.warn(err.message);
-    });
+    fetch(url)
+      .then(res => {
+        if (!res.ok) throw new Error("Failed to fetch!");
+        return res.json();
+      })
+      .then(dataObj => {
+        console.log(dataObj);
+      })
+      .catch(err => {
+        console.warn(err);
+      }) 
 
-  //The code below will always fail.
-  // let response = fetch(url);
-  // let dataobj = response.json();
-  // console.log(dataobj);
 }
 
-function fred(resp) {}
